b5c7134ed9147a57bf655b7674c2896c902844ca
dmpapazog <dmpapazog@gmail.com>
2019-03-23 21:57:20 +0200

    team main

---
+++ src/Team/Team-main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "include/Player/Player.h"
+#include "include/Player/Defender.h"
+#include "include/Team/Team.h"
+
+using namespace std;
+
+int main()
+{
+    Player k("Urko Pardo", 25, 190, 88, 31, false);
+    Defender l("Katsiabis", 33, 190, 88, 31, false, 50, 3, 130, 100, 230, 175);
+
+    cout << k.getGrade() << endl;
+    cout << l.getGrade() << endl;
+    
+    Team iraklis("Iraklis", 1908);
+    iraklis.addPlayer(&k);
+    iraklis.addPlayer(&l);
+
+    Player *a;
+    a = iraklis.getPlayer(0);
+    if (a != nullptr) {
+        cout << a->getAge() << endl;
+    }
+    cout << iraklis.avgAge() << endl;
+    cout << iraklis.sumGrades() << endl;
+
+    return EXIT_SUCCESS;
+}
