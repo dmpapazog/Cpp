af418cb48d95562e19ee27716ca0ffb223c19b8f
dmpapazog <dmpapazog@gmail.com>
2019-04-06 21:30:53 +0300

    formatted

--- include/Heap/Heap.h
+++ include/Heap/Heap.h
@@ -1,43 +1,44 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-template<class T, class Comparator>
+template <class T, class Comparator>
 class Heap {
-    private:
-        // The full size of the heap.
-        int capacity;
-        
-        // The number of the elements in the heap.
-        int size;
-        T  *data;
+private:
+    // The full size of the heap.
+    int capacity;
 
-        void heapifyUp();
-        void heapifyDown();
-        void swap(T& a, T& b);
-    public:
-        Heap();
-        Heap(const int& size);
-        Heap(const Heap& old);
-        ~Heap();
+    // The number of the elements in the heap.
+    int size;
+    T* data;
 
-        // Return the index of the left child.
-        int   getLeft(const int& parent) const;
-        // Return the index of the right child.
-        int  getRight(const int& parent) const;
-        // Return the index of the parent.
-        int getParent(const int& child)  const;
+    void heapifyUp();
+    void heapifyDown();
+    void swap(T& a, T& b);
 
-        // Return false if the heap is full.
-        bool insert(const T& insertion);
-        
-        // If the heap is not empty, save the
-        // top of the heap and return true.
-        bool extract(T& out);
-        
-        // If the heap is not empty, delete
-        // the top node and return true.
-        bool removeTop();
-        void show() const;
+public:
+    Heap();
+    Heap(const int& size);
+    Heap(const Heap& old);
+    ~Heap();
+
+    // Return the index of the left child.
+    int   getLeft(const int& parent) const;
+    // Return the index of the right child.
+    int  getRight(const int& parent) const;
+    // Return the index of the parent.
+    int getParent(const int& child)  const;
+
+    // Return false if the heap is full.
+    bool insert(const T& insertion);
+
+    // If the heap is not empty, save the
+    // top of the heap and return true.
+    bool extract(T& out);
+
+    // If the heap is not empty, delete
+    // the top node and return true.
+    bool removeTop();
+    void show() const;
 };
 
 #include "Heap.hpp"
